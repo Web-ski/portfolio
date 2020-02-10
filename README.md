91a175744a7cec304a8fe912ee0a2948d2a7460e
Paweł <pawel.nieczuja@onet.eu>
2020-02-10 16:32:05 +0100

    Add webpack

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Used commands:
 - `npm run structure` (only one, start of the project)
-- `gulp copy` - copy ready files to dist folder
+- `gulp copy` - copy ready files to dist folder,
+- `npm run dev` - runs webpack
--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var paths = {
   src: 'src/**/*',
   srcHTML: 'src/**/*.html',
   srcCSS: 'src/**/*.css',
-  srcJS: 'src/**/*.js',
+  //srcJS: 'src/**/*.js',
   srcSVG: 'src/**/*.svg',
   srcPNG: 'src/**/*.png',
   dist: 'dist',
   distHTML: 'dist/**/*.html',
   distCSS: 'dist/**/*.css',
-  distJS: 'dist/**/*.js',
+  //distJS: 'dist/**/*.js',
   distSVG: 'dist/**/*.svg',
   distPNG: 'dist/**/*.png'
 };
@@ -26,9 +26,9 @@ gulp.task('html', () => {
 gulp.task('css', () => {
   return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.dist));
 });
-gulp.task('js', () => {
-  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.dist));
-});
+// gulp.task('js', () => {
+//   return gulp.src(paths.srcJS).pipe(gulp.dest(paths.dist));
+// });
 gulp.task('svg', () => {
   return gulp.src(paths.srcSVG).pipe(gulp.dest(paths.dist));
 });
@@ -37,4 +37,4 @@ gulp.task('png', () => {
 });
 
 /*łączenie zadań*/
-gulp.task('copy', gulp.series(gulp.parallel('html', 'css', 'js', 'svg', 'png')));
+gulp.task('copy', gulp.series(gulp.parallel('html', 'css', 'svg', 'png')));
