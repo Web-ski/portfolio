1d68c3e4edfad3c03c3a12ced975c86a72022463
Paweł <pawel.nieczuja@onet.eu>
2020-02-10 16:02:42 +0100

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # portfolio
+
+Used commands:
+- `npm run structure` (only one, start of the project)
+- `gulp copy` - copy ready files to dist folder
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+//deklaracje zmiennych
+var gulp = require('gulp');
+var paths = {
+  src: 'src/**/*',
+  srcHTML: 'src/**/*.html',
+  srcCSS: 'src/**/*.css',
+  srcJS: 'src/**/*.js',
+  srcSVG: 'src/**/*.svg',
+  srcPNG: 'src/**/*.png',
+  dist: 'dist',
+  distHTML: 'dist/**/*.html',
+  distCSS: 'dist/**/*.css',
+  distJS: 'dist/**/*.js',
+  distSVG: 'dist/**/*.svg',
+  distPNG: 'dist/**/*.png'
+};
+
+//definicje tasków
+gulp.task('hello', () => {
+  console.log('Hello You ;)');
+});
+
+gulp.task('html', () => {
+  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.dist));
+});
+gulp.task('css', () => {
+  return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.dist));
+});
+gulp.task('js', () => {
+  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.dist));
+});
+gulp.task('svg', () => {
+  return gulp.src(paths.srcSVG).pipe(gulp.dest(paths.dist));
+});
+gulp.task('png', () => {
+  return gulp.src(paths.srcPNG).pipe(gulp.dest(paths.dist));
+});
+
+/*łączenie zadań*/
+gulp.task('copy', gulp.series(gulp.parallel('html', 'css', 'js', 'svg', 'png')));
