6100f6ad9bed3ed848135a7cb7347629b5fb953b
Paweł <pawel.nieczuja@onet.eu>
2020-02-14 20:18:06 +0100

    Update addStains js

--- src/js/addStain.js
+++ src/js/addStain.js
@@ -3,6 +3,8 @@ const stainPage = document.getElementsByClassName("page__stain");
 const stainsTextArr = Array.from(stainText);
 const stainsPageArr = Array.from(stainPage);
 
+const colorsArr = ['rgba(255, 0, 0, 0.3)', 'rgba(255, 55, 0, 0.3)', 'rgba(255, 0, 50, 0.3)', 'rgba(155, 0, 150, 0.3)']
+
 
 //deklaracja klasy
 class Stain {
@@ -11,10 +13,13 @@ class Stain {
     this.index = index;
   }
 
+  color1() {
+    let nmbr = Math.floor(Math.random() * (10 - 6));
+    return colorsArr[nmbr];
+  };
 
   //addAnimations(item, index);
 }
-
 
 //kontrukcja funcji rysującej
 /*function addAnimations(item, index) {
@@ -25,83 +30,89 @@ class Stain {
 }*/
 
 //kontrukcja funcji rysującej
-function addSmallStain(item, index, startX, startY) {
+function addSmallStain(item, index, startX, startY, colorM) {
 
-  let color1 = function () {
-    let x = 'rgba(255, 0, 0, 0.3)';
-    return x;
-  };
-  let color2 = function () {
-    let x = 'rgba(240, 248, 255, 0.5)';
-    return x;
-  };
+  let color1 = colorM;
 
   let x = item.offsetWidth;
   let y = item.offsetHeight;
-
 
   let pointX = Math.ceil(Math.random() * (10 - 8) + 2) / 10;
   let pointY = Math.ceil(Math.random() * (10 - 8) + 2) / 10;
 
   //points
-  /* boki a-b, b-c itd. i proporcje do nich */
   let pointA = x * startX;
   let pointB = y * startY;
-
-  /* boki a-b, b-c itd. i proporcje do nich */
   let pointC = x * (pointX + pointX);
   let pointD = y * (startY);
-
-  /* boki a-b, b-c itd. i proporcje do nich */
   let pointE = x * (pointX + pointX);
   let pointF = y * (pointY + pointY);
-
-  /* boki a-b, b-c itd. i proporcje do nich */
   let pointG = x * (startX);
   let pointH = y * (pointY + pointY);
-
-  console.log((pointC /2));
-
-  // setInterval(function() {
-  //   let nmbr = Math.floor(Math.random() *10);
-  //   nmbr > 6 ? startX = startX++ *1 : startX = startX-- * 1; 
-
-  //   console.log(startX)
 
   /*koordynaty łuków*/
   let AB = (pointC - pointA);
   let FD = (pointF - pointD);
   let EG = (pointE - pointG);
   let HB = (pointH - pointB);
-  
-  let arc1X = (pointA + pointC)/2;
-  let arc1Y = pointD -FD/2;
 
-  let arc2X = pointC + AB/2;
-  let arc2Y = (pointH + pointD)/2;
 
-  let arc3X = (pointE + pointG)/2;
-  let arc3Y = pointH + HB/2;
-
-  let arc4X = pointG - (EG)/2;
-  let arc4Y = (pointH + pointB)/2;
-
+  //setInterval(function () {
+  //   let nmbr = Math.floor(Math.random() *10);
+  //   nmbr > 6 ? startX = startX++ *1 : startX = startX-- * 1; 
+  //}, 50)
   const ctx = item.getContext("2d");
 
-  ctx.fillStyle = color1();
-  ctx.beginPath();
-  ctx.moveTo(pointA, pointB);
-  ctx.quadraticCurveTo(arc1X, arc1Y, pointC, pointD);
-  ctx.quadraticCurveTo(arc2X, arc2Y, pointE, pointF);
-  //ctx.lineTo(pointC, pointD);
-  //ctx.lineTo(pointE, pointF);
-  ctx.quadraticCurveTo(arc3X, arc3Y, pointG, pointH);
-  ctx.quadraticCurveTo(arc4X, arc4Y, pointA, pointB);
-  //ctx.lineTo(pointG, pointH);
-  //ctx.lineTo(pointA, pointB);
-  ctx.fill();
+  let pPlus = 1; //x / 100 * 20;
+  let z = -1;
 
-  //}, 50)
+  function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    let arc1X = ((pointA + pointC) / 2);
+    let arc1Y = (pointD - FD / 2);
+
+    let arc2X = (pointC + AB / 2);
+    let arc2Y = ((pointH + pointD) / 2);
+
+    let arc3X = (pointE) - ((pointE - pointG)/2);
+    let arc3Y = pointH + HB / 2;
+
+    let arc4X = (pointG - (EG) / 2);
+    let arc4Y = (pointH) - ((pointH - pointB) / 2);
+
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
+    ctx.beginPath();
+    ctx.moveTo(pointA, pointB);
+    ctx.quadraticCurveTo(arc1X, arc1Y, pointC, pointD);
+    ctx.quadraticCurveTo(arc2X, arc2Y, pointE, pointF);
+    ctx.quadraticCurveTo(arc3X, arc3Y, pointG, pointH);
+    ctx.quadraticCurveTo(arc4X, arc4Y, pointA, pointB);
+    ctx.fill();
+
+    /*switch (z) {
+      case -1:
+        if (pPlus < 600) {
+          pPlus = pPlus - z;
+        } else if (pPlus === 600) {
+          z = 1;
+        };
+        break;
+      case 1:
+        if (pPlus > 1) {
+          pPlus = pPlus - z;
+        } else if (pPlus === 1) {
+          z = -1;
+        };
+        break;
+    }*/
+
+
+    console.log(pPlus)
+  }
+
+  animate();
 }
 
 
@@ -119,8 +130,13 @@ function addStains(collection) {
       return startY;
     }
 
+    let color2 = function () {
+      let x = 'rgba(240, 248, 255, 0.5)';
+      return x;
+    };
+
     let elem = new Stain(item, index);
-    addSmallStain(elem.item, elem.index, randomX(), randomY());
+    addSmallStain(elem.item, elem.index, randomX(), randomY(), elem.color1);
   })
 }
 
