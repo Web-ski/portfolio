01bfdb22a873b3457f5411c2196c925f31ae727d
Paweł <pawel.nieczuja@onet.eu>
2020-02-27 22:10:02 +0100

    Add first hero content

--- src/js/addHeroElems.js
+++ src/js/addHeroElems.js
@@ -12,13 +12,47 @@ const addHeroElems = function () {
   const addArticles = function () {
     let article = document.createElement('article');
     article.setAttribute('class', 'article');
-    article.style.backgroundColor = randomColor();
+    //article.style.backgroundColor = randomColor();
     hero.appendChild(article);
   }
 
   for (let i = 0; i <= 40; i++) {
     addArticles();
   }
+
+  //tworzenie elementów
+  const elemsArr = [];
+
+  class Elem {
+    constructor(parentIndex, tagName, className, content) {
+      this.parentIndex = parentIndex;
+      this.tagName = tagName;
+      this.className = className;
+      this.content = content;
+      elemsArr.push(this);
+    }
+  }
+
+  const brand = new Elem(0, 'div', 'brand', {'text':'Web-ski'});
+
+
+  const addElemsContent = function (collection) {
+
+    const addElem = function (item) {
+      let parent = hero.children[item.parentIndex];
+      let child = document.createElement(item.tagName);
+      parent.appendChild(child);
+      child.setAttribute('class', item.className);
+      item.content && (child.textContent = item.content.text);
+      console.log(child)
+    }
+
+    collection.map((item) => {
+      item.tagName && addElem(item);
+    })
+  }
+
+  addElemsContent(elemsArr);
 
 }
 
--- src/js/moveHeroElems.js
+++ src/js/moveHeroElems.js
@@ -1,25 +1,18 @@
 const moveHeroElems = function () {
 
   const hero = document.querySelector('.hero');
+  const articles = hero.getElementsByClassName('article');
+  const articlesArr = Array.from(articles);
 
-  const colorArr = ['azure', 'beige', 'gainsboro', 'lightgrey', 'lavenderblush', 'grey'];
-
-  const randomColor = function () {
-    let color = colorArr[Math.floor(Math.random() * colorArr.length)];
-    return color;
-  }
-
-  const addArticles = function () {
-    let article = document.createElement('article');
-    article.setAttribute('class', 'article');
-    article.style.backgroundColor = randomColor();
-    hero.appendChild(article);
-  }
-
-  for (let i = 0; i <= 40; i++) {
-    addArticles();
-  }
+  articlesArr.map((item, index) => {
+    item.addEventListener('mouseover', function(event) {
+      item.classList.toggle('red');
+    });
+    item.addEventListener('mouseleave', function(event) {
+      item.classList.toggle('red');
+    });
+  })
 
 }
 
-addHeroElems();
+moveHeroElems();
