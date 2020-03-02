b715c19811935b91213efec2390a7317257d0f87
Paweł <pawel.nieczuja@onet.eu>
2020-03-02 15:03:35 +0100

    Add tiles animation

--- src/js/moveHeroElems.js
+++ src/js/moveHeroElems.js
@@ -5,16 +5,20 @@ const moveHeroElems = function () {
   const boxesArr = Array.from(boxes);
 
   boxesArr.map((item, index) => {
-    item.addEventListener('mouseover', function(event) {
-      item.classList.toggle('red');
-      item.children[0] && item.children[0].classList.toggle('hide');
+    item.addEventListener('mouseover', function (event) {
+      //item.classList.contains('tile') ? item.classList.toggle('tileBack');
+      item.classList.toggle('tile');
+      //item.classList.toggle('red');
+      //item.children[0] && item.children[0].classList.toggle('hide');
       console.log(item.children[0] && item.children[0]);
     });
-    /*
+
     item.addEventListener('mouseleave', function(event) {
-      item.classList.toggle('red');
+      setTimeout(function() {
+        item.classList.toggle('tile');
+      }, 1000)
     });
-    */
+
   })
 }
 
