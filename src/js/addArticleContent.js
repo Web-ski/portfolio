94f42f23b7ba1bb35b2811780e1c34145223e2b4
Paweł <pawel.nieczuja@onet.eu>
2020-03-17 18:58:55 +0100

    Add fetch for texts

--- src/js/addArticleContent.js
+++ src/js/addArticleContent.js
@@ -3,30 +3,33 @@ const addArticlesContent = function () {
   const main = document.querySelector('.main');
   const sections = Array.from(main.getElementsByClassName('section'));
   const textsArr = [];
-
-  class Text {
-    constructor(text) {
-      this.text = text;
-      textsArr.push(this)
-    }
-  }
-
-  //pobieranie tekstu
-  // const getTexts = function() {
-  // }
-  
-
+ 
   //tworzenie artykułów
   const addText = function (collection) {
     collection.map((item, index) => {
       let article = item.querySelector('.description');
-      console.log(article)
-
+      let articleText = document.createElement('p');
+      articleText.setAttribute('class', 'article__text');
+      article.appendChild(articleText);
     })
   }
-
+  
   addText(sections);
-
+  
+  //pobieranie tekstu
+  const getTexts = function () {
+    let articles = Array.from(main.getElementsByClassName('description'));
+    fetch('http://localhost/my-test/portfolio/portfolio/src/js/articles.JSON')
+      .then(resp => resp.json())
+      .then(data => {
+        let texts = data.texts;
+        texts.map((item, index) => {
+          articles[index].querySelector('.article__text').textContent = item.text;
+        })
+      });
+  }
+  
+  getTexts();
 }
 
 addArticlesContent();
