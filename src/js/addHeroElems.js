231d2f28d109413544aa204a2e596e31d1a181be
Paweł <pawel.nieczuja@onet.eu>
2020-03-26 11:27:58 +0100

    Remove art section

--- src/js/addHeroElems.js
+++ src/js/addHeroElems.js
@@ -166,7 +166,7 @@ const addHeroElems = function () {
   const portfolioTextsArr1 = ['portfolio', 'paweł', 'nieczuja-', 'ostrowski'];
   const portfolioTextsArr2 = ['of'];
   const portfolioTextsArr3 = ['menu'];
-  const portfolioTextsArr4 = ['ux', 'ui', 'webs', 'repo', 'art', 'cv', 'find'];
+  const portfolioTextsArr4 = ['ux', 'ui', 'webs', 'repo', 'cv', 'find'];
 
   portfolioTextsArr1.map((item, index) => addPortfolioText(item, index));
   addPortfolioText(portfolioTextsArr2[0], 4);
