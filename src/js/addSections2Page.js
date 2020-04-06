31ebb9bdd26c86c03774dde41bcfe9f8663a6234
Paweł <pawel.nieczuja@onet.eu>
2020-04-06 22:09:17 +0200

    Add section function

--- src/js/addSections2Page.js
+++ src/js/addSections2Page.js
@@ -4,12 +4,24 @@ const addSections2Page = function () {
 
   let url = `http://localhost/my-test/portfolio/portfolio/src/js/${pageName}.json`;
 
+  
+  //dodawanie elementów
+  const addElems = function(parent, elem, name) {
+    elem = document.createElement(elem);
+    elem.setAttribute('class', name);
+    parent.appendChild(elem);
+    return elem;
+  }
+  
   //tworzenie sekcji
-
-  const addSections = function(collection) {
+  const addSections = function (collection, parent) {
     collection.map(item => {
-      console.log(item)
-
+      let elem = document.createElement(Object.keys(item)[0]);
+      elem.setAttribute('class', Object.values(item)[0]);
+      item.text !== undefined && (elem.textContent = item.text);
+      item.src !== undefined && (elem.setAttribute('src', `./images/photos/${item.src}`));
+      parent.appendChild(elem);
+      item.children !== undefined && (addSections(item.children, elem));
     })
   }
 
@@ -19,7 +31,7 @@ const addSections2Page = function () {
     fetch(url)
       .then(resp => resp.json())
       .then(data => {
-        addSections(data)
+        addSections(data, main)
       })
       .catch(function (error) {
         console.log('Looks like there was a problem: \n', error);
@@ -27,7 +39,6 @@ const addSections2Page = function () {
   }
 
   getContent();
-
 }
 
 addSections2Page();
