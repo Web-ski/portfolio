a6126d321f36aa7c8b6d2b8914fecfba33ac655d
Paweł <pawel.nieczuja@onet.eu>
2020-04-09 12:45:05 +0200

    Update photoViewer

--- src/js/addSections2Page.js
+++ src/js/addSections2Page.js
@@ -4,21 +4,27 @@ const addSections2Page = function () {
 
   let url = `http://localhost/my-test/portfolio/portfolio/src/js/${pageName}.json`;
 
-  //dodawanie elementów
-  const addElems = function (parent, elem, name) {
-    elem = document.createElement(elem);
-    elem.setAttribute('class', name);
-    parent.appendChild(elem);
-    return elem;
+  //dodawanie data-viewer
+  const setPhotoViewer = function (item) {
+    let dataName = Object.keys(item)[1];
+    let viewerName = Object.values(item)[1];
+
+    let dataElems = Array.from(main.querySelectorAll(`[${dataName}-${viewerName}]`));
+    dataElems.map((item, index) => {
+      let name = viewerName + index;
+      let elem = document.createElement('script');
+      elem.setAttribute('type', 'text/javascript');
+      elem.textContent = `photoViewer("${name}")`;
+      document.body.appendChild(elem);
+    })
   }
 
-  //dodawanie data-viewer
-  const addPhotoViewer = (item,index) => {
-    let name = item + index;
-    let elem = document.createElement('script');
-    elem.setAttribute('type', 'text/javascript');
-    elem.textContent = `photoViewer("${name}")`;
-    document.body.appendChild(elem);
+  const addPhotoViewer = (collection, parent) => {
+    collection.map((item, index) => {
+      item.section !== undefined && (addPhotoViewer(item.children, item.section));
+      item.article !== undefined && (addPhotoViewer(item.children, item.article));
+      item.data !== undefined && (setPhotoViewer(item));
+    })
   }
 
   //tworzenie sekcji
@@ -31,7 +37,7 @@ const addSections2Page = function () {
       item.data !== undefined && (elem.setAttribute(`data-${item.data}`, item.data + index));
       parent.appendChild(elem);
       item.children !== undefined && (addSections(item.children, elem));
-      parent.children[0].getAttribute(`data-${item.data}`) !== null && (addPhotoViewer(item.data, index));
+      //parent.children[0].getAttribute(`data-${item.data}`) !== null && (addPhotoViewer(item.data, index));
     });
   }
 
@@ -41,6 +47,7 @@ const addSections2Page = function () {
       .then(resp => resp.json())
       .then(data => {
         addSections(data, main);
+        addPhotoViewer(data, main);
       })
       .catch(function (error) {
         console.log('Looks like there was a problem: \n', error);
--- src/js/photoViewer.js
+++ src/js/photoViewer.js
@@ -3,19 +3,33 @@ const photoViewer = function (name) {
 
   const main = document.querySelector(".main2Page");
   const photoBox = main.querySelector(`[data-viewer="${name}"]`);
-  //const parentArticle = photoBox.parentElement;
+  const parentArticle = photoBox.parentElement;
+  const parentArticleChildren = Array.from(parentArticle.children);
 
   //tworzenie sekcji
-  const addViewer = function (elem) {
+  const addViewer = function (collection) {
+      collection.shift();
 
-    let parent = elem.parentElement;
-    let photos = parent.children;
-    console.log(photos)
+      console.log(collection[0].getAttributeNode('src').value)
+      collection[0].classList.add('article__photo--active');
+      photoBox.style.backgroundImage = `url(${collection[0].getAttributeNode('src').value})`;
     //elem.style.backgroundImage =  
+
+    collection.map((item,index) => {
+
+      item.addEventListener("click", function(){
+        collection.map((item)=> {
+          item.classList.contains('article__photo--active') === true && item.classList.remove('article__photo--active');
+        });
+        item.classList.add('article__photo--active');
+        photoBox.style.backgroundImage = `url(${item.getAttributeNode('src').value})`;  
+      })
+    });
 
   }
 
-  addViewer(photoBox);
+  addViewer(parentArticleChildren);
 }
 
 //photoViewer();
+//object.addEventListener("load", function)
