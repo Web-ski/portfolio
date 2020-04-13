f504a42c5a9677e4418bcf189b151fb7fdcdecf8
Paweł <pawel.nieczuja@onet.eu>
2020-04-13 19:33:26 +0200

    Add content

--- src/js/photoViewer.js
+++ src/js/photoViewer.js
@@ -1,5 +1,5 @@
 const photoViewer = function (name) {
-  console.log(name)
+  //console.log(name)
 
   const main = document.querySelector(".main2Page");
   const photoBox = main.querySelector(`[data-viewer="${name}"]`);
@@ -8,27 +8,34 @@ const photoViewer = function (name) {
 
   //tworzenie sekcji
   const addViewer = function (collection) {
-      collection.shift();
+    collection.shift();
 
-      console.log(collection[0].getAttributeNode('src').value)
-      collection[0].classList.add('article__photo--active');
-      photoBox.style.backgroundImage = `url(${collection[0].getAttributeNode('src').value})`;
+    //console.log(collection[0].getAttributeNode('src').value)
+    collection[0].classList.add('article__photo--active');
+    photoBox.style.backgroundImage = `url(${collection[0].getAttributeNode('src').value})`;
     //elem.style.backgroundImage =  
 
-    collection.map((item,index) => {
-
-      item.addEventListener("click", function(){
-        collection.map((item)=> {
+    collection.map((item, index) => {
+      item.addEventListener("click", function () {
+        collection.map((item) => {
           item.classList.contains('article__photo--active') === true && item.classList.remove('article__photo--active');
         });
         item.classList.add('article__photo--active');
-        photoBox.style.backgroundImage = `url(${item.getAttributeNode('src').value})`;  
+        photoBox.style.backgroundImage = `url(${item.getAttributeNode('src').value})`;
       })
     });
-
   }
 
+
   addViewer(parentArticleChildren);
+
+  // function addPopup(elem) {
+  //   elem.addEventListener("click", function () {
+  //     alert(elem)
+  //   })
+  // }
+
+  // addPopup(photoBox)
 }
 
 //photoViewer();
