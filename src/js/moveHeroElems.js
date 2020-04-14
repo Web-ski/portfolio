a9245b8c8fc46b6dd74db8d453443cbacf67e4d1
Paweł <pawel.nieczuja@onet.eu>
2020-04-14 16:17:50 +0200

    Hide hero picture after menu click

--- src/js/moveHeroElems.js
+++ src/js/moveHeroElems.js
@@ -3,9 +3,11 @@ const moveHeroElems = function () {
   const hero = document.querySelector('.hero');
   const boxes = hero.getElementsByClassName('box');
   const menu = hero.getElementsByClassName('menu');
+  const picture = hero.getElementsByClassName('picture');
   const menusArr = Array.from(menu);
   const boxesArr = Array.from(boxes);
-
+  const pictureArr = Array.from(picture);
+  
   const column = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   //obracanie i ukrywanie
@@ -13,7 +15,7 @@ const moveHeroElems = function () {
     for (let i = row; i < boxesArr.length; i = i + 10) {
       setTimeout(function () {
         let child = boxesArr[i].children[0];
-        if (typeof child === 'undefined' || child.classList.contains('brand') !== true) {
+        if (typeof child === 'undefined' || (child.classList.contains('brand') !== true)) {
           boxesArr[i].classList.contains('tile') ? boxesArr[i].classList.toggle('tileBack') : boxesArr[i].classList.toggle('tile');
           typeof boxesArr[i].children[0] !== 'undefined' && setTimeout(function() {boxesArr[i].children[0].classList.toggle('hide')}, 400);
           typeof boxesArr[i].children[1] !== 'undefined' && setTimeout(function() {boxesArr[i].children[1].classList.toggle('hide')}, 400);
