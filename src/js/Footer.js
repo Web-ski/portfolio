949eb4c6d8611048d50b4b46596608d1f90a25c9
Paweł <pawel.nieczuja@onet.eu>
2020-05-28 12:52:49 +0200

    Add Footer js

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,2 +1,13 @@
 
+class Footer extends React.Component {
+  render() {
+    return <section className="section"></section>;
+  }
+}
 
+const footer = <Footer />
+
+ReactDOM.render(
+  footer,
+  document.getElementById('footer')
+);
