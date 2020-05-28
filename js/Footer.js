80a92f6974f0a3f9d3c6022d2de8f68637483a25
Paweł <pawel.nieczuja@onet.eu>
2020-05-28 15:19:18 +0200

    Add footer content

--- js/Footer.js
+++ js/Footer.js
@@ -6,8 +6,122 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Footer = function (_React$Component) {
-  _inherits(Footer, _React$Component);
+var FOOTER_TEXT_AUTHOR = "2020, by Pawel Nieczuja-Ostrowski";
+var URL = "content/articles.json";
+
+var FooterText = function (_React$Component) {
+  _inherits(FooterText, _React$Component);
+
+  function FooterText() {
+    _classCallCheck(this, FooterText);
+
+    return _possibleConstructorReturn(this, (FooterText.__proto__ || Object.getPrototypeOf(FooterText)).apply(this, arguments));
+  }
+
+  _createClass(FooterText, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "p",
+        { className: "article__text" },
+        this.props.text
+      );
+    }
+  }]);
+
+  return FooterText;
+}(React.Component);
+
+var FooterLink = function (_React$Component2) {
+  _inherits(FooterLink, _React$Component2);
+
+  function FooterLink() {
+    _classCallCheck(this, FooterLink);
+
+    return _possibleConstructorReturn(this, (FooterLink.__proto__ || Object.getPrototypeOf(FooterLink)).apply(this, arguments));
+  }
+
+  _createClass(FooterLink, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "a",
+        { className: "footer__link" },
+        this.props.text
+      );
+    }
+  }]);
+
+  return FooterLink;
+}(React.Component);
+
+function createElems(props) {
+  return console.log(props) //!!!!tu robię
+  ;
+}
+
+var FooterInfo = function (_React$Component3) {
+  _inherits(FooterInfo, _React$Component3);
+
+  function FooterInfo(props) {
+    _classCallCheck(this, FooterInfo);
+
+    var _this3 = _possibleConstructorReturn(this, (FooterInfo.__proto__ || Object.getPrototypeOf(FooterInfo)).call(this, props));
+
+    _this3.state = { data: [] };
+    return _this3;
+  }
+
+  _createClass(FooterInfo, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this4 = this;
+
+      fetch(URL).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        return _this4.setState({ data: json });
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "article",
+        { id: this.props.id, className: this.props.className },
+        createElems(this.state.data)
+      );
+    }
+  }]);
+
+  return FooterInfo;
+}(React.Component);
+
+var FooterAuthor = function (_React$Component4) {
+  _inherits(FooterAuthor, _React$Component4);
+
+  function FooterAuthor() {
+    _classCallCheck(this, FooterAuthor);
+
+    return _possibleConstructorReturn(this, (FooterAuthor.__proto__ || Object.getPrototypeOf(FooterAuthor)).apply(this, arguments));
+  }
+
+  _createClass(FooterAuthor, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "article",
+        { id: this.props.id, className: this.props.className },
+        React.createElement(FooterText, { text: FOOTER_TEXT_AUTHOR })
+      );
+    }
+  }]);
+
+  return FooterAuthor;
+}(React.Component);
+
+var Footer = function (_React$Component5) {
+  _inherits(Footer, _React$Component5);
 
   function Footer() {
     _classCallCheck(this, Footer);
@@ -18,7 +132,12 @@ var Footer = function (_React$Component) {
   _createClass(Footer, [{
     key: "render",
     value: function render() {
-      return React.createElement("section", { className: "section" });
+      return React.createElement(
+        "section",
+        { className: "footer" },
+        React.createElement(FooterInfo, { id: "footer-info", className: "article" }),
+        React.createElement(FooterAuthor, { id: "footer-author", className: "article" })
+      );
     }
   }]);
 
--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,7 +1,57 @@
 
+const FOOTER_TEXT_AUTHOR = "2020, by Pawel Nieczuja-Ostrowski";
+const URL = "content/articles.json";
+
+class FooterText extends React.Component {
+  render() {
+    return <p className="article__text">{this.props.text}</p>
+  }
+}
+
+class FooterLink extends React.Component {
+  render() {
+    return <a className="footer__link">{this.props.text}</a>
+  }
+}
+
+function createElems(props) {
+  return (
+    console.log(props) //!!!!tu robię
+    )
+}
+
+class FooterInfo extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    fetch(URL).then(response => response.json()).then(json => this.setState({ data: json }))
+  }
+
+  render() {
+    return <article id={this.props.id} className={this.props.className}>
+      {createElems(this.state.data)}
+    </article>;
+  }
+}
+
+class FooterAuthor extends React.Component {
+  render() {
+    return <article id={this.props.id} className={this.props.className}>
+      <FooterText text={FOOTER_TEXT_AUTHOR} />
+    </article>;
+  }
+}
+
 class Footer extends React.Component {
   render() {
-    return <section className="section"></section>;
+    return <section className="footer">
+      <FooterInfo id="footer-info" className="article" />
+      <FooterAuthor id="footer-author" className="article" />
+    </section>;
   }
 }
 
