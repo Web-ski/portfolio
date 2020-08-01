6e927101d9727e8fabddf9d90741e9c628e733a6
Paweł <pawel.nieczuja@onet.eu>
2020-08-01 19:34:34 +0200

    Add StartHeader

--- src/components/Headers/StartHeader.js
+++ src/components/Headers/StartHeader.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, useParams } from "react-router-dom";
-import './SecondHeader.scss';
+import './StartHeader.scss';
 import { BRAND_NAME } from '../../constans';
 import Brand from '../Brand/Brand'
 import PageTitle from '../Titles/PageTitle'
 
 class StartHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
+    const viewportOrientation = viewportHeight > viewportWidth ? 'mobile' : 'desktop';
+    this.state = { orientation: viewportOrientation, boxSize: 1 }
+  }
+
+  componentDidMount(){
+   this.elems(this.state.orientation);
+  }
+
+  elems(orientation) {
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
+    let boxSize;
+    orientation === 'mobile' && (boxSize = (viewportWidth / 9) && this.boxGenerator(boxSize, viewportHeight));
+    orientation === 'desktop' && (boxSize = (viewportHeight / 10) && this.boxGenerator(boxSize, viewportWidth));
+  }
+
+  boxGenerator(size) {
+    console.log('hje')
+  }
 
   render() {
+    //console.log(this.state.orientation)
     return <header className="second-header">
-      <PageTitle text={"HERO"} />
+      <div className="hero">
+
+      </div>
     </header>
   }
 }
