89016b3544b265376ddd1359ab78e4856303a49c
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-08-06 11:19:57 +0200

    Add handleClick

--- src/components/Headers/StartHeader.js
+++ src/components/Headers/StartHeader.js
@@ -4,14 +4,23 @@ import { BRAND, MENU, PAGE } from '../../constans';
 import HeroItem from './HeroItem';
 
 class StartHeader extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { isToggleOn: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({ isToggleOn: !state.isToggleOn }));
+  }
 
   render() {
-
+    const toggle = this.state.isToggleOn;
     return <header className="start-header">
       <div className="hero">
-        <div className="hero__item">
+        <div className={"hero__item" + (toggle ? "" : " red")}>
           <p className={BRAND.name}>{BRAND.text}</p>
-          <button className={MENU.name}>{MENU.text}</button>
+          <button onClick={this.handleClick} className={MENU.name}>{MENU.text}</button>
         </div>
         <HeroItem />
         {PAGE.map((item, index) => <HeroItem key={"page" + index} elem={item} />)}
