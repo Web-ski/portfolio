1de9f90b8499e2d9fccb6610f4154ffd636aff7a
Paweł <pawel.nieczuja@onet.eu>
2020-09-30 17:20:27 +0200

    Add listener

--- src/components/CV/FlowToolsBox.js
+++ src/components/CV/FlowToolsBox.js
@@ -13,11 +13,26 @@ class FlowToolsBox extends React.Component {
     }
   };
 
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.reloadArray());
+    }
+    this.reloadArray();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.reloadArray());
+  }
+
+  reloadArray() {
+    let arr = this.state.tools;
+    this.setState({ tools: arr })
+  }
 
   render() {
     return <article className={this.props.elem.flowToolsBox + " article__flow"}>
       <div className={this.props.elem.children[0].div}>
-        {(this.state.tools).map((item, index) => <FlowItem key={"tool" + index} elem={[this.state.tools[index], index]} /> )}
+        {(this.state.tools).map((item, index) => <FlowItem key={"tool" + index} elem={[this.state.tools[index], index]} />)}
       </div>
     </article>;
   }
