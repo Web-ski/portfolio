b82208ede0cf7e45b0f1185200680e9f82ad60e6
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-10-12 01:14:13 +0200

    Add section navigation

--- src/components/Sections/SectionStart.js
+++ src/components/Sections/SectionStart.js
@@ -5,10 +5,23 @@ import Article from './Article';
 class SectionStart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      sectionOn: "articleOn",
-      sectionDisplay: ''
-    }
+    this.state = {
+      sectionOn: "",
+      articleNumber: 0,
+      sectionDisplay: ""
+    };
+  }
+
+  articleActive(nmbr) {
+    return (nmbr === this.state.articleNumber ? this.setState({ sectionOn: "articleOn" }) : this.setState({ sectionOn: "" }));
+  }
+
+  componentDidMount() {
+    this.setState({ sectionOn: "articleOn" })
+  }
+
+  handleClick = (nmbr) => {
+    this.setState({ articleNumber: nmbr })
   }
 
   render() {
@@ -16,9 +29,18 @@ class SectionStart extends React.Component {
     return (<section id={elem.id} className={elem.section}>
       {(elem.children !== undefined) && (
         (elem.children).map((item, index) => {
-          if (item.article !== undefined) { return <Article key={item.toString() + index} elem={[item, this.state.sectionOn]} /> };
+          if (item.article !== undefined) 
+          { return <Article key={item.toString() + index} 
+          elem={[item, (this.state.articleNumber === index ? "articleOn" : "")]} /> };
         })
       )}
+      <nav className="section-navigation">
+        {(elem.children !== undefined) && (
+          (elem.children).map((item, index) => {
+            if (item.article !== undefined) { return <button onClick={this.handleClick(index)} key={item.toString() + index} className={"navigation__btn"}></button> };
+          })
+        )}
+      </nav>
     </section>)
   }
 }
