a18a0f5a5ccb83d6e9f016ee8f78ba8b8435e12f
Paweł <pawel.nieczuja@onet.eu>
2020-10-13 16:12:46 +0200

    Ass Swiper React

--- src/components/Sections/SectionStart.js
+++ src/components/Sections/SectionStart.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './SectionsStart.scss';
 import Article from './Article';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, A11y } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+//import 'swiper/components/scrollbar/scrollbar.scss';
+
+SwiperCore.use([Pagination]);
 
 class SectionStart extends React.Component {
   constructor(props) {
@@ -20,26 +28,49 @@ class SectionStart extends React.Component {
   render() {
     const elem = this.props.elem;
     return (<section id={elem.id} className={elem.section}>
-      {(elem.children !== undefined) && (
-        (elem.children).map((item, index) => {
-          if (item.article !== undefined) 
-          { return <Article key={item.toString() + index} 
-          elem={[item, (this.state.articleOn === index ? "articleOn" : "")]} /> };
-        })
-      )}
-      <nav className="section-navigation">
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        //width={100}
+        //navigation
+        pagination={{ clickable: true }}
+        //scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
         {(elem.children !== undefined) && (
           (elem.children).map((item, index) => {
-            if (item.article !== undefined) 
-            { return <button onClick={() => this.articleActive(index)}
-            key={item.toString() + index} 
-            className={"navigation__btn " + (this.state.btnOn === index ? "navigation__btn--active" : "")
-          }></button> };
+            if (item.article !== undefined) {
+              return <SwiperSlide style={{width: "100%"}} key={'slide' + index}>
+                <Article key={item.toString() + index}
+                  elem={[item, "articleOn"]} />
+              </SwiperSlide>
+            };
           })
         )}
-      </nav>
+
+      </Swiper>
     </section>)
   }
 }
 
 export default SectionStart;
+
+// {(elem.children !== undefined) && (
+//   (elem.children).map((item, index) => {
+//     if (item.article !== undefined) 
+//     { return <Article key={item.toString() + index} 
+//     elem={[item, (this.state.articleOn === index ? "articleOn" : "")]} /> };
+//   })
+// )}
+// <nav className="section-navigation">
+//   {(elem.children !== undefined) && (
+//     (elem.children).map((item, index) => {
+//       if (item.article !== undefined) 
+//       { return <button onClick={() => this.articleActive(index)}
+//       key={item.toString() + index} 
+//       className={"navigation__btn " + (this.state.btnOn === index ? "navigation__btn--active" : "")
+//     }></button> };
+//     })
+//   )}
+// </nav>
