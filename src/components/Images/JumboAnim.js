29175d0197f91097a67a89b6faeb018a938d6297
Paweł <pawel.nieczuja@onet.eu>
2020-11-09 11:26:30 +0100

    Add cnavas points

--- src/components/Images/JumboAnim.js
+++ src/components/Images/JumboAnim.js
@@ -6,13 +6,12 @@ function rect(props) {
   ctx.fillRect(x, y, width, height);
 }
 
-function circle(props) {
-  const { ctx, x, y, width, height } = props;
-  ctx.fillRect(x, y, width, height);
-  ctx.beginPath();
-  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-  ctx.stroke();
-}
+// function circle(props) {
+//   const { ctx, x, y, radius, startAngle, endAngle} = props;
+//   ctx.arc(x, y, radius, startAngle, endAngle);
+//   //ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+// }
+
 class JumboAnim extends React.Component {
   componentDidMount() {
     this.updateCanvas();
@@ -22,16 +21,31 @@ class JumboAnim extends React.Component {
   }
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
-    ctx.clearRect(0, 0, 300, 300);
+    ctx.clearRect(0, 0, 280, 280);
     // draw children “components”
+    //ctx.beginPath();
+    // circle({ ctx, x: 100, y: 75, radius: 50, startAngle: 0, endAngle: 2*Math.PI})
+    // ctx.fill();
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
 
-    // rect({ ctx, x: 10, y: 10, width: 5, height: 5 });
-    // rect({ ctx, x: 110, y: 110, width: 5, height: 5 });
+    for (let i = 0; i < 1000; i += 1) {
+      ctx.fillStyle = "white";
+
+        rect({ ctx, x: getRandomInt(280), y: getRandomInt(280), width: 4, height: 4 });
+
+
+    }
+    //rect({ ctx, x: 10, y: 10, width: 4, height: 4 });
+    //rect({ ctx, x: 110, y: 110, width: 4, height: 4 });
   }
+
+
 
   render() {
     return <aside className={this.props.name}>
-      <canvas ref="canvas" width={300} height={300} />
+      <canvas ref="canvas" width={280} height={280} className="snow" />
     </aside>
   }
 }
